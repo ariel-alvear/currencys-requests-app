4696af84ad16b7807234386178d2f5b3bdf0dce0
Ariel Alvear <aalvear@buk.cl>
2022-09-23 23:35:30 -0300

    we bring the uf value via api request

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,5 +4,17 @@ class HomeController < ApplicationController
   end
 
   def uf_value
+    api_url = "https://mindicador.cl/api/uf/#{(@date_today)}"
+    response = HTTParty.get(api_url)
+    responsetohash = JSON.parse(response.read_body)
+    if responsetohash['serie'][0].nil?
+      return render json: {mensaje:"Valor no existe en esa fecha"}
+    else 
+      @uf_value = responsetohash['serie'][0]['valor']
+    end
+  end
+
+  def date_today
+    @date_today = Time.now.strftime("%m-%d-%Y")
   end
 end
