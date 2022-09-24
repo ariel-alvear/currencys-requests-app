94cb40aee9127a3b0dc6cf4bbd1952a3ea4ac909
Ariel Alvear <aalvear@buk.cl>
2022-09-23 23:39:32 -0300

    added dolar value the same way of uf value

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -14,6 +14,17 @@ class HomeController < ApplicationController
     end
   end
 
+  def dolar_value
+    api_url = "https://mindicador.cl/api/dolar/#{(@date_today)}"
+    response = HTTParty.get(api_url)
+    responsetohash = JSON.parse(response.read_body)
+    if responsetohash['serie'][0].nil?
+      return render json: {mensaje:"Valor no existe en esa fecha"}
+    else 
+      @dolar_value = responsetohash['serie'][0]['valor']
+    end
+  end
+
   def date_today
     @date_today = Time.now.strftime("%m-%d-%Y")
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   get '/home/uf_value', to: 'home#uf_value'
+  get '/home/dolar_value', to: 'home#dolar_value'
 
   root "home#index"
 end
